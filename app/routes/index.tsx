e240fb53d3d631ad336b28b71cf1274c6d15d106
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2021-12-22 15:46:47 -0500

    attempt to load some data from KV

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,23 @@
+import { useLoaderData, LoaderFunction } from "remix";
+
+type LoaderData = {
+    data: any;
+};
+
+export async function loader<LoaderFunction>() {
+    const val = BLOG.get("foo");
+
+    return {
+        data: val,
+    };
+}
+
 export default function Index() {
+    const data = useLoaderData();
     return (
         <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-            Hello,  World!
+            Hello, World!
+            {JSON.stringify(data)}
         </div>
     );
 }
